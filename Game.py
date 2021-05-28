fd02a9d3787cc6b189ca9a19bbaf5f2a8abe0425
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2021-05-28 18:15:33 -0300

    Novo tamanho de fase 160x96

--- Game.py
+++ Game.py
@@ -5,9 +5,9 @@ from random import randint
 def player(refX, refY):
 
     if playerLookLeft:
-        playerImg = spritesheet.subsurface((0,0,RESOLUTION,RESOLUTION))
+        playerImg = spritesheet.subsurface((0, 0, RESOLUTION, RESOLUTION))
     else:
-        playerImg = spritesheet.subsurface((0,32,RESOLUTION,RESOLUTION))
+        playerImg = spritesheet.subsurface((0, 32, RESOLUTION, RESOLUTION))
     playerImg = pygame.transform.scale(playerImg, (RESOLUTION * SCALE, RESOLUTION * SCALE))
     screen.blit(playerImg, (refX*SCALE, refY*SCALE))
 
@@ -16,28 +16,28 @@ def npcGoldy(refX, refY):
     if npcLeft == 1:
         goldyImg = spritesheet.subsurface((5 * 32, 32, RESOLUTION, RESOLUTION))
     else:
-        goldyImg = spritesheet.subsurface((5*32,0,RESOLUTION,RESOLUTION))
+        goldyImg = spritesheet.subsurface((5*32, 0, RESOLUTION, RESOLUTION))
     goldyImg = pygame.transform.scale(goldyImg, (RESOLUTION * SCALE, RESOLUTION * SCALE))
-    screen.blit(goldyImg, (refX*SCALE,refY*SCALE))
+    screen.blit(goldyImg, (refX*SCALE, refY*SCALE))
 
 def trueDoor(refX, refY):
-    trueDoorImg = spritesheet.subsurface((32,2*32,RESOLUTION,RESOLUTION))
+    trueDoorImg = spritesheet.subsurface((32, 2*32, RESOLUTION, RESOLUTION))
     trueDoorImg = pygame.transform.scale(trueDoorImg, (RESOLUTION * SCALE, RESOLUTION * SCALE))
-    screen.blit(trueDoorImg, (refX*SCALE,refY*SCALE))
+    screen.blit(trueDoorImg, (refX*SCALE, refY*SCALE))
 
 def fakeDoor(refX, refY):
-    fakeDoorImg = spritesheet.subsurface((32,2*32,RESOLUTION,RESOLUTION))
+    fakeDoorImg = spritesheet.subsurface((32, 2*32, RESOLUTION, RESOLUTION))
     fakeDoorImg = pygame.transform.scale(fakeDoorImg, (RESOLUTION * SCALE, RESOLUTION * SCALE))
     screen.blit(fakeDoorImg, (refX*SCALE, refY*SCALE))
 
 def message(txt, posX, posY):
 
     text = font.render(txt, True, (255, 255, 255))
-    screen.blit(text, (posX*SCALE,posY*SCALE))
+    screen.blit(text, (posX*SCALE, posY*SCALE))
 
-def isColliding(x1 , y1 , x2 , y2):
+def isColliding(x1, y1, x2, y2):
     distance = math.sqrt(((x2+RESOLUTION/2) - (x1+RESOLUTION/2)) ** 2 + ((y2+RESOLUTION/2) - (y1+RESOLUTION/2)) ** 2)
-    if distance < 45:
+    if distance < 20:
         return True
     else:
         return False
@@ -48,7 +48,7 @@ def showHint():
     global textHint
 
     if not hintShowed:
-        i = randint(0,9)
+        i = randint(0, 9)
         if exitLeft == 1:
             textHint = hintLeft[i]
             hintShowed = True
@@ -82,7 +82,7 @@ def nextLevel():
     counter = 30
     textCounter = str(counter).rjust(3)
     exitLeft = randint(0, 1)
-    npcLeft = randint(0,1)
+    npcLeft = randint(0, 1)
     hintShowed = False
 
 pygame.init()
@@ -90,18 +90,18 @@ pygame.init()
 spritesheet = pygame.image.load("spritesheet.png")
 
 
-WIDTH = 512
-HEIGHT = 256
-SCALE = 2
+WIDTH = 160
+HEIGHT = 96
+SCALE = 6
 RESOLUTION = 32
 
-hintLeft = ["esquerda1","esquerda2","esquerda3","esquerda4","esquerda5","esquerda6","esquerda7","esquerda8","esquerda9","esquerda10"]
-hintRight = ["direita1","direita2","direita3","direita4","direita5","direita6","direita7","direita8","direita9","direita10"]
+hintLeft = ["esquerda1", "esquerda2", "esquerda3", "esquerda4", "esquerda5", "esquerda6", "esquerda7", "esquerda8", "esquerda9", "esquerda10"]
+hintRight = ["direita1", "direita2", "direita3", "direita4", "direita5", "direita6", "direita7", "direita8", "direita9", "direita10"]
 
 stage = pygame.image.load("stage.png")
 stage = pygame.transform.scale(stage, (WIDTH*SCALE, HEIGHT*SCALE))
 font = pygame.font.SysFont("Consolas", 20)
-screen = pygame.display.set_mode((WIDTH*SCALE, HEIGHT*SCALE),0,32)
+screen = pygame.display.set_mode((WIDTH*SCALE, HEIGHT*SCALE), 0, 32)
 timer = pygame.time.Clock()
 level = 1
 MAX_LEVEL = 5
@@ -113,22 +113,22 @@ pygame.time.set_timer(pygame.USEREVENT, 1000)
 counter = 60
 textCounter = str(counter).rjust(3)
 exitLeft = randint(0, 1)
-npcLeft = randint(0,1)
+npcLeft = randint(0, 1)
 
 playerX0 = (WIDTH/2) - (RESOLUTION/2)
-playerY0 = 200
+playerY0 = 60
 
 playerX = playerX0
 playerY = playerY0
 
-goldyX = 100
-goldyY = 180
+goldyX = 8
+goldyY = 59
 
 trueDoorX = 0
-trueDoorY = 128
+trueDoorY = 33
 
 fakeDoorX = 0
-fakeDoorY = 128
+fakeDoorY = 33
 
 hintShowed = False
 textHint = ""
@@ -145,19 +145,19 @@ playerLookLeft = True
 while running:
     if gameState == "NORMAL":
         screen.fill((0, 0, 0))
-        screen.blit(stage,(0,0))
+        screen.blit(stage, (0, 0))
 
         if exitLeft == 1:
-            trueDoorX = 176
-            fakeDoorX = 306
+            trueDoorX = 41
+            fakeDoorX = 85
         else:
-            trueDoorX = 306
-            fakeDoorX = 176
+            trueDoorX = 85
+            fakeDoorX = 41
 
         if npcLeft == 1:
-            goldyX = 100
+            goldyX = 8
         else:
-            goldyX = 380
+            goldyX = 120
 
         for event in pygame.event.get():
             if event.type == pygame.USEREVENT and runningCounter:
@@ -202,21 +202,21 @@ while running:
         if playerLeft:
             playerX -= speed
 
-        if playerX > 390:
-            playerX = 390
-        if playerX < 90:
-            playerX = 90
+        if playerX > 108:
+            playerX = 108
+        if playerX < 20:
+            playerX = 20
         if playerY > (HEIGHT - RESOLUTION):
             playerY = (HEIGHT - RESOLUTION)
-        if playerY < 140:
-            playerY = 140
+        if playerY < 36:
+            playerY = 36
 
         if playerAction:
-            if isColliding(playerX, playerY, trueDoorX,trueDoorY):
+            if isColliding(playerX, playerY, trueDoorX, trueDoorY):
                 nextLevel()
             elif isColliding(playerX, playerY, fakeDoorX, fakeDoorY):
                 gameState = "GAME OVER"
-            elif isColliding(playerX,playerY, goldyX, goldyY):
+            elif isColliding(playerX, playerY, goldyX, goldyY):
                 showHint()
                 print("dica")
 
@@ -225,14 +225,14 @@ while running:
         npcGoldy(goldyX, goldyY)
         player(playerX, playerY)
 
-        message(textLevel, 30, 10)
-        message(textCounter, WIDTH - 30, 10)
+        message(textLevel, 10, 5)
+        message(textCounter, WIDTH - 20, 5)
 
         if hintShowed:
-            message(textHint, WIDTH/2 , 10)
+            message(textHint, WIDTH/2, 5)
     elif gameState == "GAME OVER":
-        screen.fill((0,0,0))
-        message("Voce perdeu!",WIDTH/2,HEIGHT/2)
+        screen.fill((0, 0, 0))
+        message("Voce perdeu!", WIDTH/2, HEIGHT/2)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -242,4 +242,5 @@ while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
     pygame.display.update()
